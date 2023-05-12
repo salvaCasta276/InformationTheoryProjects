375067ad0c3410b65f4b585ba7353543b0c129a7
salvaCasta276 <salCas276@gmail.com>
2023-05-12 00:45:22 -0300

    Finished project 3

--- Project3/generator.py
+++ Project3/generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 #Function used to calculate values of the type G(1, m) where m is any value equal or greater than 1
 #It can be proven that G(1, m) has dimensions (m+1)x(2^m), we use this to calculate the dimension of the 0 block
@@ -18,11 +19,21 @@ def G(r, m):
 
 m = 3
 generator = G(1, m)
+
+bin = {0,1}
+bin_power = itertools.product(bin, repeat=2**(m-1))
+codewords = [np.dot(np.array(u), generator)%2 for u in bin_power]
+for codeword in codewords:
+    print(codeword)
+
 errors = np.identity(2 ** m)
 syndromes = np.dot(errors, generator.T)%2
 codeword = generator[1]
+
 print(generator)
 print(np.dot(generator, generator.T) % 2)
 print(syndromes)
 #We get the syndrom which we can use to find the error and add it to the codeword
 print(np.dot((codeword + errors[0]), generator.T)%2)
+#Add the error to intermediate value
+print(((codeword + errors[0]) + errors[0])%2)
